69366f367c98f2898dbe95075feafef8c02d5514
RAYMONDNJOROGE <raymondnjoroge20@gmail.com>
2025-05-19 15:19:33 +0300

    commit2

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ return [
     'consumerSecret' => 'hu1EnuMQO4asAmvwqRn65c5OZwDqTnYAz9hA5NQaL0GopQQOAkuJjRhGWFtOAiak',
     'businessShortCode' => '174379',
     'passkey' => 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
-    'callbackUrl' => 'https://shrouded-meadow-45282-36291630ca1c.herokuapp.com/callback.php', // Replace with your actual domain
+    'callbackUrl' => 'https://ancient-shore-02928-a2081f813c36.herokuapp.com/', // Replace with your actual domain
 ];
 ?>
