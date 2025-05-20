2cd455fad5813191007f4fd473f47280d82d09eb
RAYMONDNJOROGE <raymondnjoroge20@gmail.com>
2025-05-20 14:31:04 +0300

    sdfghas

--- index.php
+++ index.php
@@ -520,27 +520,6 @@ async function pollRealTimeSTKStatus(checkoutID) {
 
             const { ResultCode, statusMessage } = await statusRes.json();
 
-            closePopup("stk-okay-pop"); // Close STK okay popup before showing final result
-
-            // Open correct popup based on STK status
-            if (ResultCode === 0) {
-                clearInterval(pollInterval);
-                openPopup("pay-accepted-pop"); // STK push accepted
-                setTimeout(() => closePopup("pay-accepted-pop"), 4000);
-            } else if (ResultCode === 1032) {
-                clearInterval(pollInterval);
-                openPopup("pay-cancel-pop"); // STK push cancelled
-                setTimeout(() => closePopup("pay-cancel-pop"), 4000);
-            } else {
-                openPopup("stk-pending-pop"); // Waiting for response
-                setTimeout(() => closePopup("stk-pending-pop"), 4000);
-            }
-        } catch (err) {
-            clearInterval(pollInterval);
-            closePopup("stk-okay-pop");
-            openPopup("stk-error-pop");
-            setTimeout(() => closePopup("stk-error-pop"), 4000);
-        }
     }, 1000); // Poll every second
 }
                     </script>
